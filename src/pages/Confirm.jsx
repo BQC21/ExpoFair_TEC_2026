36f2aed4de313e85a47af24ae92ae5e5f57325d1
Braulio <braulio.quine@utec.edu.pe>
2026-08-17 14:22:34 -0500

    content 2 confirm page

---
+++ src/pages/Confirm.jsx
@@ -0,0 +1,20 @@
+function Confirm() {
+    return (
+        <>
+        <div>
+            {/* Agregar check*/}
+
+            <img src={Tec_ES_logo} alt="Logo TEC" />
+            <h1>Gracias por participar</h1>
+            <p>Tus respuestas fueron registradas exitosamente</p>
+
+            <footer>
+                tec-energysolutions.pe - Lima, Perú
+            </footer>
+
+        </div>
+        </>
+    );
+}
+
+export default Confirm;
